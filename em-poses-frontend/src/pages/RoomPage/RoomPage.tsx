13b0f94d6fd1f536c6c6af7e3729125b14f3a79f
Carles Fèlix <carlesfelix@gmail.com>
2021-08-02 18:36:50 +0200

    prevent multiple docket connections

--- em-poses-frontend/src/pages/RoomPage/RoomPage.tsx
+++ em-poses-frontend/src/pages/RoomPage/RoomPage.tsx
@@ -1,49 +1,49 @@
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import { io, Socket } from 'socket.io-client';
+import { io } from 'socket.io-client';
 import PageLayout from '../../components/PageLayout';
 import { SocketsProvider } from '../../contexts/sockets';
 import environment from '../../environment';
 import { getRoomPageRoutes } from './helpers/room-page-routes';
 
+const songRequestsSocket = io(`${environment.REACT_APP_SOCKET_BASE_URI}/song-requests`, {
+  autoConnect: false,
+  forceNew: true,
+  auth: (cb) => {
+    cb({
+      room: `token -> ${Date.now()}`
+    })
+  }
+});
+
 export default function RoomPage() {
-  const [ songRequestsSocket, setSongRequestsSocket ] = useState<Socket | undefined>();
   const { url, params } = useRouteMatch<{ id: string }>();
   const { id: roomId } = params;
   const roomPageRoutes = getRoomPageRoutes(url);
   useEffect(() => {
-    console.log(environment.REACT_APP_SOCKET_BASE_URI);
-    setSongRequestsSocket(io(`${environment.REACT_APP_SOCKET_BASE_URI}/song-requests`, {
-      auth: {
-        room: roomId
-      }
-    }));
+    songRequestsSocket.connect();
     return () => {
-      songRequestsSocket && songRequestsSocket.disconnect();
+      songRequestsSocket.disconnect();
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ roomId ]);
   return (
       <div className="RoomPage">
-        {
-          !!songRequestsSocket && (
-            <SocketsProvider songRequestsSocket={songRequestsSocket}>
-              <Switch>
-                {
-                  roomPageRoutes.map((roomPageRoute, iRoomPageRoute) => (
-                    <Route path={`${url}${roomPageRoute.path}`} key={iRoomPageRoute}>
-                      <PageLayout toolbarTitle={roomPageRoute.toolbarTitle} routes={roomPageRoutes} links={roomPageRoute.links} toolbarLinkBack={roomPageRoute.toolbarLinkBack}>
-                        <Suspense fallback={false}>
-                          <roomPageRoute.Component />
-                        </Suspense>
-                      </PageLayout>
-                    </Route>
-                  ))
-                }
-              </Switch>
-            </SocketsProvider>
-          )
-        }
+        <SocketsProvider songRequestsSocket={songRequestsSocket}>
+          <Switch>
+            {
+              roomPageRoutes.map((roomPageRoute, iRoomPageRoute) => (
+                <Route path={`${url}${roomPageRoute.path}`} key={iRoomPageRoute}>
+                  <PageLayout toolbarTitle={roomPageRoute.toolbarTitle} routes={roomPageRoutes} links={roomPageRoute.links} toolbarLinkBack={roomPageRoute.toolbarLinkBack}>
+                    <Suspense fallback={false}>
+                      <roomPageRoute.Component />
+                    </Suspense>
+                  </PageLayout>
+                </Route>
+              ))
+            }
+          </Switch>
+        </SocketsProvider>
       </div>
   );
 }
