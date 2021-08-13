bec8274df637c5b7f9a4933e9ca43e47b28dddce
Carles Fèlix <carlesfelix@gmail.com>
2021-08-13 22:33:17 +0200

    api cors

--- em-poses-backend/src/api/app/index.ts
+++ em-poses-backend/src/api/app/index.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import environment from '../../environment';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
