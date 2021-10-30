150fea393e539598783ba161a2fd33ec043610af
Carles Fèlix <carlesfelix@gmail.com>
2021-10-30 14:02:13 +0200

    make a song request page layout

--- em-poses-frontend/src/pages/MakeASongRequestPage/MakeASongRequestPage.tsx
+++ em-poses-frontend/src/pages/MakeASongRequestPage/MakeASongRequestPage.tsx
@@ -1,11 +1,36 @@
+import { useForm } from 'react-hook-form';
+import ControlledInput from '../../components/forms/ControlledInput';
 import PageLayout from '../../components/PageLayout';
 import './MakeASongRequestPage.scss';
 
 export default function MakeASongRequestPage() {
+  const { control } = useForm();
   return (
     <PageLayout toolbarTitle="Make a song request" toolbarLinkBack="/">
       <div className="MakeASongRequestPage">
-        Make a song request page
+        <form autoComplete="off" className="search-songs-form sub-toolbar">
+          <div className="page-content">
+            <ControlledInput
+              control={control}
+              field={{
+                type: 'inputText',
+                props: { placeholder: 'Search a song' }
+              }}
+              name="query"
+              className="search-input"
+            />
+          </div>
+        </form>
+        <div className="page-content song-request-content">
+          content here
+        </div>
+        <div className="song-request-footer sub-toolbar">
+          <div className="page-content">
+            <button className="btn btn-primary">
+              Send request
+            </button>
+          </div>
+        </div>
       </div>
     </PageLayout>
   );
