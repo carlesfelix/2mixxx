bd3b4eb4c0bd2af082dc89960ba73cfb8eaf270a
Carles Fèlix <carlesfelix@gmail.com>
2021-11-16 19:33:25 +0100

    mysql cert

--- backend/src/data-sources/sequelize-mysql/sequelize.connection.ts
+++ backend/src/data-sources/sequelize-mysql/sequelize.connection.ts
@@ -1,17 +1,31 @@
-import { Sequelize } from 'sequelize';
+import { Options, Sequelize } from 'sequelize';
+import { readFileSync } from 'fs';
+
 import environment from '../../environment';
 
-const dbLog = environment.NODE_ENV === 'development' && console.log;
+const development = environment.NODE_ENV === 'development';
+const dbLog = development && console.log;
+
+const sequelizeOptions: Options = {
+  host: environment.MYSQL_HOST,
+  dialect: 'mysql',
+  port: environment.MYSQL_PORT,
+  logging: dbLog,
+  ssl: environment.MYSQL_SSL
+};
+
+if (environment.MYSQL_SSL && environment.MYSQL_CERT_PATH) {
+  sequelizeOptions.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: [ readFileSync(environment.MYSQL_CERT_PATH, 'utf8') ]
+    }
+  };
+}
 
 export default new Sequelize(
   environment.MYSQL_DATABASE,
   environment.MYSQL_USER,
   environment.MYSQL_PASSWORD,
-  {
-    host: environment.MYSQL_HOST,
-    dialect: 'mysql',
-    port: environment.MYSQL_PORT,
-    ssl: environment.MYSQL_SSL,
-    logging: dbLog
-  }
+  sequelizeOptions
 );
