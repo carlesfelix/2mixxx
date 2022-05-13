56d50a7534d39a119b33347c3a7d5c412e7e481e
Carles Fèlix <carlesfelix@gmail.com>
2022-05-13 17:27:09 +0200

    release 1.0.0-alpha.2

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -7,7 +7,7 @@ const environment = {
   REACT_APP_AUTH0_DOMAIN: env('REACT_APP_AUTH0_DOMAIN'),
   REACT_APP_AUTH0_CLIENT_ID: env('REACT_APP_AUTH0_CLIENT_ID'),
   REACT_APP_API_BASE_URL: env('REACT_APP_API_BASE_URL'),
-  VERSION: '1.0.0-alpha.1',
+  VERSION: '1.0.0-alpha.2',
   repository: 'https://github.com/carlesfelix/2mixxx'
 };
 
