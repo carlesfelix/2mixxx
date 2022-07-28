c4ee1c9e4f8c11889ae95028846db9e9f11d8ac4
Carles Fèlix <carlesfelix@gmail.com>
2022-07-28 17:22:36 +0200

    add rate limiter to socket server

--- backend/src/socket/app/namespaces/main/index.ts
+++ backend/src/socket/app/namespaces/main/index.ts
@@ -3,12 +3,14 @@ import authMid from '../../middlewares/auth.mid';
 import joinRoomUsersMid from './middlewares/join-room-users.mid';
 import errorListeners from './listeners/error.listeners';
 import songRequestListeners from './listeners/song-request.listeners';
+import rateLimiterMid from '../../middlewares/rate-limiter.mid';
 
 function MainNsp(io: Server): Namespace {
   const mainNamespace = io.of('/');
   mainNamespace.use(authMid);
   mainNamespace.use(joinRoomUsersMid);
   mainNamespace.on('connection', socket => {
+    socket.use(rateLimiterMid(socket));
     songRequestListeners(io, socket);
     errorListeners(socket);
   });
--- backend/src/socket/app/namespaces/moderate-rooms/index.ts
+++ backend/src/socket/app/namespaces/moderate-rooms/index.ts
@@ -1,5 +1,6 @@
 import { Namespace, Server } from 'socket.io';
 import authMid from '../../middlewares/auth.mid';
+import rateLimiterMid from '../../middlewares/rate-limiter.mid';
 import errorListeners from './listeners/error.listeners';
 import songRequestListeners from './listeners/song-request.listeners';
 import joinModeratorToRoomMid from './middlewares/join-moderator-to-room.mid';
@@ -9,6 +10,7 @@ function ModerateRoomsNsp(io: Server): Namespace {
   moderateRoomNamespace.use(authMid);
   moderateRoomNamespace.use(joinModeratorToRoomMid);
   moderateRoomNamespace.on('connection', socket => {
+    socket.use(rateLimiterMid(socket));
     songRequestListeners(io, socket);
     errorListeners(socket);
   });
