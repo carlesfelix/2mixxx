0813165d5f586b9cf9a0202f575c81b908eb9968
Carles Fèlix <carlesfelix@gmail.com>
2022-08-09 23:58:31 +0200

    improve useSocketConnectionManager

--- frontend/src/hooks/useSocketConnectionManager.ts
+++ frontend/src/hooks/useSocketConnectionManager.ts
@@ -1,25 +1,27 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { logoutGuestMeAction } from '../contexts/me/me.actions';
 import { removeRoomUserAction, useRoomUser } from '../contexts/room-user';
 import { getGuestToken } from '../services/room-user-auth';
+import usePrevious from './usePrevious';
 
-export default function useSocketConnectionManager(uri: string): Socket | undefined {
-  const [ socket, setSocket ] = useState<Socket | undefined>(undefined);
+export default function useSocketConnectionManager(uri: string): Socket {
+  const previousUri = usePrevious(uri);
   const { state: roomUserState, dispatch: roomUserAuthDispatch } = useRoomUser();
   const { isAuthenticated, isLoading, getIdTokenClaims } = useAuth0();
   const registeredLogged = !isLoading && isAuthenticated;
   const guestLogged =  !roomUserState.inProgress &&
     roomUserState.isAuthenticated;
 
-  useEffect(() => {
-    const nextSocket = io(uri, { autoConnect: false });
+  const buildConnection = useCallback((
+    uriConnection: string
+  ) => {
     function onError(): void {
       removeRoomUserAction(roomUserAuthDispatch);
       logoutGuestMeAction();
     }
-    setSocket(nextSocket);
+    const nextSocket = io(uriConnection, { autoConnect: false });
     if (registeredLogged) {
       getIdTokenClaims().then(idToken => {
         if (!idToken) {
@@ -45,12 +47,25 @@ export default function useSocketConnectionManager(uri: string): Socket | undefi
     } else {
       console.warn('Socket not connected. Auth is required');
     }
-    return () => {
-      nextSocket.disconnect();
-    }
+    return nextSocket;
   }, [
     registeredLogged, guestLogged, getIdTokenClaims,
-    uri, roomUserAuthDispatch
+    roomUserAuthDispatch
   ]);
+  const [ socket, setSocket ] = useState<Socket>(() => (
+    buildConnection(uri)
+  ));
+
+  useEffect(() => {
+    if (previousUri !== undefined && uri !== previousUri) {
+      setSocket(buildConnection(uri));
+    }
+  }, [ buildConnection, uri, previousUri, setSocket ]);
+
+  useEffect(() => {
+    return () => {
+      socket.disconnect();
+    }
+  }, [ socket ]);
   return socket;
 }
