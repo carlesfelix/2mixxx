7679f02dfd024a7063c02f9770d5afcb7c1692cc
Carles Fèlix <carlesfelix@gmail.com>
2022-11-01 00:27:57 +0100

    update control panel layout

--- frontend2/src/components/layout/ControlPanelLayout/ControlPanelLayout.tsx
+++ frontend2/src/components/layout/ControlPanelLayout/ControlPanelLayout.tsx
@@ -1,18 +1,23 @@
 import { ControlPanelLayoutProps } from "./types";
 import "./ControlPanelLayout.css";
-import OutlinedButton from "@/components/atoms/OutlinedButton";
+import IconButton from "@/components/atoms/IconButton";
+import { ReactComponent as MenuIcon } from '@/assets/svg/Menu.svg';
 
 export default function ControlPanelLayout(props: ControlPanelLayoutProps) {
   const { children } = props;
   return (
     <div className="ControlPanelLayout">
       <div className="ControlPanelLayout__menu-container">
-        
-        <div className="a">
-          
+        <div className="ControlPanelLayout__menu-header">
+          <IconButton className="ControlPanelLayout__menu-btn" size="lg">
+            <MenuIcon />
+          </IconButton>
+          <div className="ControlPanelLayout__title-container">
+            <h3 className="_text _text--h3 ControlPanelLayout__title">2MIXX</h3>
+          </div>
         </div>
-        <div className="b">
-          Menu container 2
+        <div className="ControlPanelLayout__menu-content">
+          Menu content
         </div>
       </div>
       <div className="ControlPanelLayout__main-container">
