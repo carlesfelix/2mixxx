fb7245d78eda3723ccab2a6b5588512cec04d996
Carles Fèlix <carlesfelix@gmail.com>
2022-11-03 12:03:24 +0100

    add buttons sample

--- frontend2/src/pages/RegisteredUserEventsPage/RegisteredUserEventsPage.tsx
+++ frontend2/src/pages/RegisteredUserEventsPage/RegisteredUserEventsPage.tsx
@@ -1,3 +1,5 @@
+import BasicButton from "@/components/atoms/BasicButton";
+import IconButton from "@/components/atoms/IconButton";
 import OutlinedButton from "@/components/atoms/OutlinedButton";
 
 export default function RegisteredUserEventsPage() {
@@ -12,6 +14,51 @@ export default function RegisteredUserEventsPage() {
       <OutlinedButton size="lg" color="primary">
         Button
       </OutlinedButton>
+      <OutlinedButton size="sm" color="secondary">
+        Button
+      </OutlinedButton>
+      <OutlinedButton size="md" color="secondary">
+        Button
+      </OutlinedButton>
+      <OutlinedButton size="lg" color="secondary">
+        Button
+      </OutlinedButton>
+      <BasicButton size="sm" color="primary">
+        Button
+      </BasicButton>
+      <BasicButton size="md" color="primary">
+        Button
+      </BasicButton>
+      <BasicButton size="lg" color="primary">
+        Button
+      </BasicButton>
+      <BasicButton size="sm" color="secondary">
+        Button
+      </BasicButton>
+      <BasicButton size="md" color="secondary">
+        Button
+      </BasicButton>
+      <BasicButton size="lg" color="secondary">
+        Button
+      </BasicButton>
+      <IconButton size="sm" color="primary">
+        X
+      </IconButton>
+      <IconButton size="md" color="primary">
+        X
+      </IconButton>
+      <IconButton size="lg" color="primary">
+        X
+      </IconButton>
+      <IconButton size="sm" color="secondary">
+        X
+      </IconButton>
+      <IconButton size="md" color="secondary">
+        X
+      </IconButton>
+      <IconButton size="lg" color="secondary">
+        X
+      </IconButton>
     </div>
   );
 }
