7964ad4c963564edad423460a42281260dcda39e
Carles Fèlix <carlesfelix@gmail.com>
2022-11-04 10:35:03 +0100

    autofocus sidebar content

--- frontend2/src/components/atoms/Sidebar/components/SidebarContent/SidebarContent.tsx
+++ frontend2/src/components/atoms/Sidebar/components/SidebarContent/SidebarContent.tsx
@@ -1,16 +1,19 @@
 import { useFocusTrap } from "@/core/core-hooks";
 import classNames from "classnames";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { SidebarContentProps } from "../../types";
 import "./SidebarContent.css";
 
 export default function SidebarContent(props: SidebarContentProps) {
   const { children, className } = props;
   const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ref.current?.focus();
+  }, [ref]);
   useFocusTrap(ref);
   const rootClassName = classNames('SidebarContent', className);
   return (
-    <div className={rootClassName} ref={ref}>
+    <div className={rootClassName} ref={ref} tabIndex={-1} role="complementary">
       {children}
     </div>
   );
