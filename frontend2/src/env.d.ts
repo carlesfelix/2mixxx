cefaeb0c31a834b283953240e0048620bce98240
Carles Fèlix <carlesfelix@gmail.com>
2022-12-11 14:56:14 +0100

    configure environment variables

---
+++ frontend2/src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SOCKET_BASE_URI: string;
+  readonly VITE_AUTH0_DOMAIN: string;
+  readonly VITE_AUTH0_CLIENT_ID: string;
+  readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ frontend2/src/environment/environment.ts
@@ -0,0 +1,8 @@
+const ENVIRONMENT = {
+  SOCKET_BASE_URI: import.meta.env.VITE_SOCKET_BASE_URI,
+  AUTH0_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL
+}
+
+export default ENVIRONMENT
