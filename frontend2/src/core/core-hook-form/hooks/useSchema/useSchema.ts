f9afae27895395c77163f431509a3b16f676b7f3
Carles Fèlix <carlesfelix@gmail.com>
2024-01-27 20:08:30 +0100

    fix: update joi import

--- frontend2/src/core/core-hook-form/hooks/useSchema/useSchema.ts
+++ frontend2/src/core/core-hook-form/hooks/useSchema/useSchema.ts
@@ -1,9 +1,9 @@
-import { ValidationOptions, Root, defaults } from 'joi'
+import Joi, { ValidationOptions, Root } from 'joi'
 import { useMemo } from 'react'
 
 export default function useSchema (validationOptions: ValidationOptions = {}): Root {
   const schema = useMemo(() => {
-    return defaults((schema) => {
+    return Joi.defaults((schema) => {
       return schema.options(validationOptions)
     })
   }, [validationOptions])
